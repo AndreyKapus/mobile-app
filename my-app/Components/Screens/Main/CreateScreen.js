6fae5ca35958cd0238e44e67fd5bdce1870c61df
AndreyKapus <kapustnikov@ukr.net>
2023-07-04 14:34:50 +0300

    snap-btn

--- my-app/Components/Screens/Main/CreateScreen.js
+++ my-app/Components/Screens/Main/CreateScreen.js
@@ -1,11 +1,16 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Camera, CameraType } from 'expo-camera';
 
 const CreateScreen = () => {
     return (
         <View style={styles.constainer}>
-            <Text>
-                 CreateScreen
-            </Text>
+            <Camera style={styles.camera}>
+                <TouchableOpacity style={styles.snapButton}>
+                    <Text style={styles.snapText}>
+                        SNAP
+                    </Text>
+                </TouchableOpacity>
+            </Camera>
         </View>
     )
 };
@@ -13,9 +18,29 @@ const CreateScreen = () => {
 const styles = StyleSheet.create({
     constainer: {
         flex: 1,
+    },
+
+    camera: {
+        height: 300,
+        marginTop: 20,
+        alignItems: 'center'
+    },
+
+    snapButton: {
+        width: 60,
+        height: 60,
+        borderWidth: 1,
+        borderRadius: 50,
+        borderColor: '#ff0000',
+        marginTop: 220,
         justifyContent: 'center',
         alignItems: 'center',
-    }
-})
+    },
+
+    snapText: {
+        color: '#fff',
+        
+    },
+});
 
 export default CreateScreen;
