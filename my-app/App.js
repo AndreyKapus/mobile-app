cef0edf46f0f53aa115cb6111106a1f22eefd0d9
AndreyKapus <kapustnikov@ukr.net>
2023-07-06 19:33:48 +0300

    store

--- my-app/App.js
+++ my-app/App.js
@@ -2,13 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import useAuth from './router/router';
+import { Provider } from 'react-redux';
+import {store} from './Redux/store/store'
 
 export default function App() {
   const routing = useAuth({})
   return (
-    <NavigationContainer>
-      {routing}
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        {routing}
+      </NavigationContainer>
+    </Provider>
   );
 }
 
--- my-app/Components/Screens/NestedScreens/MapScreen.js
+++ my-app/Components/Screens/NestedScreens/MapScreen.js
@@ -1,5 +1,5 @@
 import { View, Text, StyleSheet } from "react-native";
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 
 const MapScreen = () => {
     return (
@@ -12,7 +12,12 @@ const MapScreen = () => {
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421,
               }}
-            />
+            >
+                <Marker coordinate={{                    
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                }}/>
+            </MapView>
 
            
         </View>
